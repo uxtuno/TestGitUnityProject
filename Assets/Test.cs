54b21def5f0e20e37ea37df313a7cc45d3f6611f
uxtuno <uxtuno664@gmail.com>
2015-10-02 08:19:10 +0900

    作成

---
+++ Assets/Test.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 position = transform.position;
+		position.x = Mathf.Cos(Time.time * 1f) * 10.0f;
+		transform.position = position;
+	}
+}
---
+++ Assets/Test2.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test2 : MonoBehaviour {
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 position = transform.position;
+		position.y = Mathf.Sin(Time.time * 1f) * 10.0f;
+		transform.position = position;
+	}
+}
