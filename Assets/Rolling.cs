c57b7302129779d359fcae0096c9dfcfdbe64a43
uxtuno <uxtuno664@gmail.com>
2015-10-08 21:33:38 +0900

    mainシーン中央に回転する正方形追加

---
+++ Assets/Rolling.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rolling : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		Vector3 angles = transform.eulerAngles;
+		angles.z = 360.0f * Mathf.Sin(Time.time * 1.0f);
+		transform.eulerAngles = angles;
+	}
+}
